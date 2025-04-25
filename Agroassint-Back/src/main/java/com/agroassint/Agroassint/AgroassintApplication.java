91214086dab40223650b86d899728721c2954596
LeonardoA <leonardoarango1803@gmail.com>
2025-04-24 19:31:05 -0500

    Backend
    back base dependecias:
    * Spring Data JPA SQL
    Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.
    * Lombok Developer Tools
    Java annotation library which helps to reduce boilerplate code.
    * Spring Web Web
    Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.
    * MySQL Driver SQL
    MySQL JDBC driver.

---
+++ Agroassint-Back/src/main/java/com/agroassint/Agroassint/AgroassintApplication.java
@@ -0,0 +1,13 @@
+package com.agroassint.Agroassint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroassintApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroassintApplication.class, args);
+	}
+
+}
---
+++ Agroassint-Back/src/test/java/com/agroassint/Agroassint/AgroassintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agroassint.Agroassint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroassintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
