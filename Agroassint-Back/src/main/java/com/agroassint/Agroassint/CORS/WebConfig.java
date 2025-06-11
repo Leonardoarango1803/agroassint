4687c06dca954da741f1d2407128f5194324a4db
Leonardo Arango <leonardoarango1803@gmail.com>
2025-06-10 22:04:59 -0500

    WebConfig

---
+++ Agroassint-Back/src/main/java/com/agroassint/Agroassint/CORS/WebConfig.java
@@ -0,0 +1,22 @@
+package com.agroassint.Agroassint.CORS;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://leonardoarango1803.github.io")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
