e1856876a82f73fafc82855fabdcf4ee98f9207b
Leonardo Arango <leonardoarango1803@gmail.com>
2025-07-09 20:24:32 -0500

    Actualización del backend

--- Agroassint-Back/src/main/java/com/agroassint/Agroassint/CORS/WebConfig.java
+++ Agroassint-Back/src/main/java/com/agroassint/Agroassint/CORS/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOriginPatterns("https://leonardoarango1803.github.io/agroassint")
+                        .allowedOriginPatterns("https://leonardoarango1803.github.io")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
